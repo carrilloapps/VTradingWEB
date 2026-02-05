ebf44f6507d4b1c61e75d5acd8201bb35a2fc593
José Carrillo <soyjrcarrillo@gmail.com>
2026-02-05 17:11:08 -0500

    Configure Sentry to reduce tracing and replay sampling rates, disable logs, and prevent sending PII.

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,12 +9,12 @@ Sentry.init({
   dsn: "https://928901e1bbc9870e227fbc6369ec336c@o456904.ingest.us.sentry.io/4510835553730560",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Enable logs to be sent to Sentry
-  enableLogs: true,
+  enableLogs: false,
 
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  sendDefaultPii: false,
 });
